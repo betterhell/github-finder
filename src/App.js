e619024f7950af4e25c648c7399b099ed3e79769
Dmitriy betterHell <skkogame@gmail.com>
2022-10-04 18:29:46 +0300

    env fix and app.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
                               }
                             />
                             <Route path="/about" element={<About />} />
-                            <Route path='/users/:login' element={<User />} />
+                            <Route path='/user/:login' element={<User />} />
                             <Route path="/notfound" element={<Notfound />} />
                             <Route path="*" element={<Notfound />} />
                         </Routes>
